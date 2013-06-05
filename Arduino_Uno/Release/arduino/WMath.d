4ab9820747f0a92ab9e2c77933208d7fffc14bbe
Rogério Peixoto <rcbpeixoto@gmail.com>
2013-06-05 14:53:38 -0300

    Organização de projetos e pacotes
    Padronização

--- Arduino_Uno/Release/arduino/WMath.d
+++
@@ -1,2 +0,0 @@
-arduino/WMath.d: \
- /home/rcbpeixoto/IDE/arduino-1.0.4/hardware/arduino/cores/arduino/WMath.cpp
--- Arduino_Uno/Release/arduino/new.d
+++
@@ -1,5 +0,0 @@
-arduino/new.d: \
- /home/rcbpeixoto/IDE/arduino-1.0.4/hardware/arduino/cores/arduino/new.cpp \
- /home/rcbpeixoto/IDE/arduino-1.0.4/hardware/arduino/cores/arduino/new.h
-
-/home/rcbpeixoto/IDE/arduino-1.0.4/hardware/arduino/cores/arduino/new.h:
